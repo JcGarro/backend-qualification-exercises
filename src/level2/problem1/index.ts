61483c34b00d2e4633239687d92051f3029e9601
John Carlos Garro <jchgarro@iskolarngbayan.pup.edu.ph>
2024-12-29 23:20:31 +0800

    Async function fire.

--- src/level2/problem1/index.ts
+++ src/level2/problem1/index.ts
@@ -1,11 +1,26 @@
 export class ExecutionCache<TInputs extends Array<unknown>, TOutput> {
+  
+  private box: Map<string, Promise<TOutput>> = new Map();
   constructor(private readonly handler: (...args: TInputs) => Promise<TOutput>) {}
   
   async fire(key: string, ...args: TInputs): Promise<TOutput> {
     /**
      * insert your code here
      */
-  
-    return;
+    if (this.box.has(key)) {
+      
+      const bProm = this.box.get(key);
+      if (!bProm?.[Symbol.toStringTag]?.includes('Promise')) {
+        this.box.delete(key);
+      } 
+      else {
+        return bProm;
+      }
+    }
+
+    const out = await this.handler(...args);
+    this.box.set(key, Promise.resolve(out));
+    return out;    
   }
 }
+
