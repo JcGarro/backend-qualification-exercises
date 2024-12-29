c338db804a4fe2902967b31b7352957aa3513cb3
John Carlos Garro <jchgarro@iskolarngbayan.pup.edu.ph>
2024-12-29 23:22:01 +0800

    Downtime Logs
    Unable to complete sorry.

--- src/level2/problem2/index.ts
+++ src/level2/problem2/index.ts
@@ -4,5 +4,12 @@ export function merge(...args: DowntimeLogs[]): DowntimeLogs {
   /**
    * insert your code here
    */
+  const downs = args.flat();
+  downs.sort(function(x, y) {
+    return x[0].getTime() - y[0].getTime(); 
+  });
+  const merg : DowntimeLogs[];
+
+
   return [];
 }
