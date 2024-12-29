278d725e39657fd12e763a9a6880a9bbb7078f82
John Carlos Garro <jchgarro@iskolarngbayan.pup.edu.ph>
2024-12-29 23:16:40 +0800

    Creation of ObjectId Class
    Add 2 static numbers for random number and counter.
    
    Create an array of objects to be accessed later. Create number array containing the expected number of bytes per identifier. These arrays are ordered respectively.
    
    These number are converted into array of bytes before passed on buffer. Lastly it is concatenated and stored on data property.

--- src/level1/problem2/index.ts
+++ src/level1/problem2/index.ts
@@ -1,10 +1,30 @@
 export class ObjectId {
   private data: Buffer;
+  private static rand: number = Math.floor(Math.random() * Math.pow(2, 32));
+  private static count: number = Math.floor(Math.random() * Math.pow(2, 24));
 
   constructor(type: number, timestamp: number) {
     /**
      * insert your code here
      */
+    const uid = [type,timestamp,ObjectId.rand,ObjectId.count++]
+    const numbyte = [1,6,4,3]
+    const dat = []
+    
+    for (let r = 0; r < 4; r++){
+      const bytes = []
+      const byteCount = numbyte[r]
+      let val = uid[r]
+      for (let i = 0; i < byteCount; i++) {
+        bytes.unshift(val & 0xff);
+        val = val >>> 8;
+      }
+      //console.log(bytes)
+      dat.push(Buffer.from(bytes))
+    }
+    //console.log(dat)
+    this.data = Buffer.concat(dat)
+     
   }
 
   static generate(type?: number): ObjectId {
